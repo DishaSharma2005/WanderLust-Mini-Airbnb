dab5ece446901b1ba26f272652196d6b6980b219
DISHA SHARMA <sharmadisha25july@gmail.com>
2025-07-21 22:33:12 +0530

    Fixed

--- app.js
+++ app.js
@@ -103,7 +103,11 @@ app.use((err, req, res, next) => {
 
 
 
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`✅ Server running on port ${port}`);
+});
 
-app.listen(8080,() =>{
-    console.log("server is listening to port 8080");
-})
+// app.listen(8080,() =>{
+//     console.log("server is listening to port 8080");
+// })
